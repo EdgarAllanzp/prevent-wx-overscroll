d7f8cba517ea3bec6eba1e803913b043671efcc3
EdgarAllanzp <edgarallan@qq.com>
2018-07-19 11:20:03 +0800

    Minify package.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import minify from 'rollup-plugin-babel-minify';
+
 export default {
   input: 'src/index.js',
   output: {
     name: 'preventWxOverscroll',
     file: './dist/preventWxOverScroll.js',
     format: 'umd'
-  }
+  },
+  plugins: [
+    minify()
+  ]
 };
